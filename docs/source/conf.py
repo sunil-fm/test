abafc3c02466fbea068a1d2c7705c28fab8448da
ghimiresunil <sunilghimire64@gmail.com>
2025-07-08 21:17:50 +0545

    exclude __init__

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "../../src/*.py"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "__init__.py"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
