8e839799dfcaab90ff55c7672135fff83c5fb878
ghimiresunil <sunilghimire64@gmail.com>
2025-07-08 21:39:11 +0545

    update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,11 +1,3 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 import os
 import sys
 
@@ -17,19 +9,29 @@ author = "Jonathan Arias Busto"
 release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",  # For Google and NumPy style docstrings
-    "sphinx.ext.viewcode",  # Optional: to include links to source code
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",  # Optional: for summary tables
 ]
+
+# Autodoc settings
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
+autodoc_mock_imports = []  # Mock external dependencies if needed
+
+# Napoleon settings (Google/NumPy docstrings)
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = False
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# -- HTML output -------------------------------------------------
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+html_extra_path = [".nojekyll"]  # Alternative to manual touch in workflow
