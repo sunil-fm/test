c6a3f1dfeb73e51c22d65fc5753961c4c8ce335f
ghimiresunil <sunilghimire64@gmail.com>
2025-07-08 20:18:23 +0545

    test theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,29 +8,29 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src'))
 
-project = 'Documentation'
-copyright = '2024, Jonathan Arias Busto'
-author = 'Jonathan Arias Busto'
-release = '0.0.1'
+sys.path.insert(0, os.path.abspath("../../src"))
+
+project = "Documentation"
+copyright = "2024, Jonathan Arias Busto"
+author = "Jonathan Arias Busto"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',  # For Google and NumPy style docstrings
-    'sphinx.ext.viewcode'   # Optional: to include links to source code
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # For Google and NumPy style docstrings
+    "sphinx.ext.viewcode",  # Optional: to include links to source code
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx-rtd-theme"
+html_static_path = ["_static"]
