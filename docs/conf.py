12710e4f69e20ad29043e3622c3643b415b6b6ed
ghimiresunil <sunilghimire64@gmail.com>
2025-07-09 10:53:12 +0545

    fixed minor changes

--- docs/conf.py
+++ docs/conf.py
@@ -4,18 +4,28 @@ import sys
 sys.path.insert(0, os.path.abspath("../jl"))
 
 project = "jl"
-copyright = "2023, redandgreen.co.uk"
 author = "Mr Moo"
+copyright = "2023, redandgreen.co.uk"
 release = "0.0.1"
 
-
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
-
-autodoc_mock_imports = ["configs", "dynaconf"]
-
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "m2r2",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+source_suffix = [".rst", ".md"]
+master_doc = "index"
+
+autodoc_typehints = "description"
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
 
